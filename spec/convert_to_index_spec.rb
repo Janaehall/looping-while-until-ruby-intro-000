2f75a395b6272422a28e1c12dc3f36659fa22fcd
bionic-chipset-2986 <janaeelisa@gmail.com>
2019-08-06 22:22:50 +0000

    Automatically backed up by Learn

--- spec/convert_to_index_spec.rb
+++ spec/convert_to_index_spec.rb
@@ -3,6 +3,6 @@ require_relative "../lib/convert_to_index.rb"
 
 describe "#convert_to_index" do
 	it "converts the user input of 1-9 to its corresponding index value of 0-8" do
-	  expect{4}.to eq{3}
+	  expect{convert_to_index(4)}.to eq{3}
   end
 end
